1e17617491f6baa3bf468a89ca35f787d6f8e658
Ilya <mister.rezznik@gmail.com>
2018-03-02 14:52:50 +0400

    Initial commit

---
+++ src/ImageUtils.java
@@ -0,0 +1,9 @@
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Ilya on 3/2/2018.
+ */
+public class ImageUtils {
+    BufferedImage image = new BufferedImage(100, 50, BufferedImage.TYPE_INT_ARGB);
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Ilya on 3/2/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
